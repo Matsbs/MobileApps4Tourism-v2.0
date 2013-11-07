2f3ae1afc6c9b7d807636250621a84f067d78343
Mats Sandvoll <matssandvoll@gmail.com>
2013-11-07 12:20:23 +0000

    Initial Commit

---
+++ MA4TV2/MA4TV2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MA4TV2AppDelegate.h
+//  MA4TV2
+//
+//  Created by Mats Sandvoll on 07.11.13.
+//  Copyright (c) 2013 Mats Sandvoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MA4TV2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MA4TV2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MA4TV2
+//
+//  Created by Mats Sandvoll on 07.11.13.
+//  Copyright (c) 2013 Mats Sandvoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MA4TV2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MA4TV2AppDelegate class]));
+    }
+}
