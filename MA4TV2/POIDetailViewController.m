a888b1021bb3851340eddb196da33e5c072e2af1
Mats Sandvoll <matssandvoll@gmail.com>
2013-12-28 13:42:59 +0100

    commit

--- MA4TV2/POIDetailViewController.m
+++ MA4TV2/POIDetailViewController.m
@@ -82,6 +82,7 @@
             self.indexOfPOI=i;
         }
     }
+    
     if (self.indexOfPOI>0) {
         UIBarButtonItem *prevButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"prev.png"] style:UIBarButtonItemStylePlain target:self action:@selector(prevClicked:)];
         [barItems2 addObject:prevButton];
